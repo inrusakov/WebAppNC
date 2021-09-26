b9ae38c62e84788230cb4495c3c141e39ced2b87
umeaire@gmail.com <umeaire@gmail.com>
2021-09-26 22:52:01 +0300

    pojo user

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -12,10 +12,12 @@ public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
-
-    private String name;
-
+    private String name; //as login
     private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Role role;
 
     public Integer getId() {
         return id;
@@ -39,5 +41,50 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", role=" + role +
+                '}';
     }
 }
