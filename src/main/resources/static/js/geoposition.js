488373e2ea262da9a6a7c307a9a0e8a0fb91250b
inrusakov <inrusakov@edu.hse.ru>
2021-10-04 06:21:00 +0300

    geoposition.js upd

--- src/main/resources/static/js/geoposition.js
+++ src/main/resources/static/js/geoposition.js
@@ -1,4 +1,4 @@
-navigator.geolocation.getCurrentPosition(function(position) {
+async function locationSuccess(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const altitude = position.coords.altitude;
@@ -8,6 +8,25 @@ navigator.geolocation.getCurrentPosition(function(position) {
     const speed = position.coords.speed;
     const timestamp = position.timestamp;
 
-    // work with this information however you'd like!
-    console.debug(latitude+" "+longitude);
-});
+    console.debug("Location:   latitude: "+ latitude + "   longitude: "+ longitude)
+    let response = await fetch("https://api.opencagedata.com/geocode/v1/json?q="
+        +latitude
+        +"+"
+        +longitude
+        +"+&key=4abf3bea6bad4dfcbd723ed0d6283bc5");
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json);
+    } else {
+        alert("ERROR HTTP: " + response.status);
+    }
+}
+
+function locationError(error) {
+    const code = error.code;
+    const message = error.message;
+
+    // read the code and message and decide how you want to handle this!
+}
+
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
