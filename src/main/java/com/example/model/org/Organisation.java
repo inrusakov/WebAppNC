0a119cdd982e6bea6243334f63af6d8414d58e50
inrusakov <inrusakov@edu.hse.ru>
2021-10-04 09:08:51 +0300

    +organisation

--- src/main/java/com/example/model/org/Organisation.java
+++ src/main/java/com/example/model/org/Organisation.java
@@ -6,60 +6,63 @@ import com.example.model.geoposition.Position;
 
 import javax.persistence.*;
 
-//@Entity
-//public class Organisation {
-//    @Id
-//    @GeneratedValue(strategy= GenerationType.AUTO)
-//    private int orgID;
-//
-//    @OneToOne(optional = false, mappedBy = "org")
-//    private Position position;
-//
-//    private String name;
-//
-//    @OneToOne(optional = false, mappedBy = "org")
-//    private User admin;
-//
-//    @OneToOne(optional = false, mappedBy = "org")
-//    private Address address;
-//
-//    public int getOrgID() {
-//        return orgID;
-//    }
-//
-//    public void setOrgID(int orgID) {
-//        this.orgID = orgID;
-//    }
-//
-//    public Position getPosition() {
-//        return position;
-//    }
-//
-//    public void setPosition(Position position) {
-//        this.position = position;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public User getAdmin() {
-//        return admin;
-//    }
-//
-//    public void setAdmin(User admin) {
-//        this.admin = admin;
-//    }
-//
-//    public Address getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(Address address) {
-//        this.address = address;
-//    }
-//}
+@Entity
+public class Organisation {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int orgID;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "position_id", referencedColumnName = "id")
+    private Position position;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User admin;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+    public int getOrgID() {
+        return orgID;
+    }
+
+    public void setOrgID(int orgID) {
+        this.orgID = orgID;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(User admin) {
+        this.admin = admin;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
