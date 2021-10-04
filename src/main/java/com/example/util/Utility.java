1a43ec4a98cd186053d53a8766729419a89c862d
inrusakov <inrusakov@edu.hse.ru>
2021-10-04 12:03:22 +0300

    comments

--- src/main/java/com/example/util/Utility.java
+++ src/main/java/com/example/util/Utility.java
@@ -1,4 +1,25 @@
 package com.example.util;
 
 public class Utility {
+
+    /**
+     * Calculating distance between point using coords.
+     * @param lat1 First point latitude.
+     * @param lng1 First point longitude,
+     * @param lat2 Second point latitude.
+     * @param lng2 Second point longitude.
+     * @return Distance between points.
+     */
+    public static double calculateDistance(double lat1, double lng1, double lat2, double lng2){
+        double earthRadius = 6371000; //meters
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng/2) * Math.sin(dLng/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        float dist = (float) (earthRadius * c);
+
+        return dist;
+    }
 }
--- src/main/resources/static/js/geoposition.js
+++ src/main/resources/static/js/geoposition.js
@@ -8,37 +8,50 @@ async function locationSuccess(position) {
     const speed = position.coords.speed;
     const timestamp = position.timestamp;
 
+    // Console output.
     console.debug("Location:   latitude: "+ latitude + "   longitude: "+ longitude)
+    // Connecting with opencagedata service to get address, passing coords.
     let response = await fetch("https://api.opencagedata.com/geocode/v1/json?q="
         +latitude
         +"+"
         +longitude
         +"+&key=4abf3bea6bad4dfcbd723ed0d6283bc5");
+    // Checking the response.
     if (response.ok) {
         let json = await response.json();
+        // Logging in console the response from geo service;
         console.log(json);
     } else {
         alert("ERROR HTTP: " + response.status);
     }
 
+    // Creating the object passing to geo controller.
     var geoRequest = {
         //"user" : user,
         "latitude" : latitude,
         "longitude" :longitude
     }
 
+    // Creating post request with estimated info to controller.
     $.ajax({
+        // Request type.
         type: "POST",
         contentType : 'application/json; charset=utf-8',
+        // Request data type.
         dataType : 'json',
+        // Request link.
         url: "/sendGeo",
+        // Converting request object.
         data: JSON.stringify(geoRequest),
         success : function(data) {
+            // Success log.
             console.log("SUCCESS: ", data);
         },
         error : function(e) {
+            // Error log.
             console.log("ERROR: ", e);
         },
+        // Passing the csrf token.
         headers: {"X-CSRF-TOKEN": $("input[name='_csrf']").val()}
     });
 }
