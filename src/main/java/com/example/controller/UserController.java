7b90ad8cda6e223dbeedee56e4b7f8cbdf03696a
marchenkoiv <57600939+marchenkoiv@users.noreply.github.com>
2021-11-01 20:54:34 +0300

    Revert "User"

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,12 +3,11 @@ package com.example.controller;
 import com.example.model.CustomUserDetails;
 import com.example.model.Role;
 import com.example.model.User;
-import com.example.model.blog.Blog;
-import com.example.model.community.Group;
-import com.example.repos.GroupRepository;
+import com.example.repos.CommentRepository;
 import com.example.repos.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -16,8 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import java.io.File;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
-import java.util.Optional;
+import java.util.List;
 
 @AllArgsConstructor
 
@@ -28,10 +28,6 @@ public class UserController {
     private UserRepository userRepository;
 
     @Autowired
-    private GroupRepository groupRepository;
-
-
-    @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("/registration")
@@ -40,136 +36,92 @@ public class UserController {
     }
 
     @PostMapping("/registration")
-    public String registration(@RequestParam String email, @RequestParam String password, Model model) {
+    public String registration (@RequestParam String email, @RequestParam String password, Model model) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
         User userFromDB = userRepository.findByEmail(email);
-        if (userFromDB != null && (userFromDB.isActive() || userFromDB.isWasBanned())) {
+        if (userFromDB != null){
             model.addAttribute("message", "User exists!");
             return "registration";
         }
-        if (userFromDB != null && !userFromDB.isActive()) {
-            userFromDB.setPassword(password);
-            userFromDB.setPassword_encoded(bCryptPasswordEncoder.encode(password));
-            userFromDB.setActive(true);
-            userRepository.save(userFromDB);
-        } else {
-            User user = new User();
-            user.setEmail(email);
-            user.setPassword(password);
-            user.setPassword_encoded(bCryptPasswordEncoder.encode(password));
-            user.setRole(Collections.singleton(Role.USER));
-            user.setActive(true);
-            user.setWasBanned(false);
-            userRepository.save(user);
-        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+
+        user.setPassword_encoded(bCryptPasswordEncoder.encode(password));
+
+        user.setRole(Collections.singleton(Role.USER));
+        user.setActive(true);
+        userRepository.save(user);
+        int id = user.getId();
         return "redirect:/login";
     }
 
     @GetMapping("/profile")
-    public String profile(Model model) {
+    public String profile(Model model){
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof CustomUserDetails) {
+        if(principal instanceof CustomUserDetails){
             model.addAttribute("user", ((CustomUserDetails) principal).getUser());
             return "profile";
         }
         return "redirect:";
     }
 
-    @GetMapping("/editProfile")
-    public String editProfile(Model model) {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof CustomUserDetails) {
-            model.addAttribute("user", ((CustomUserDetails) principal).getUser());
-            return "editProfile";
-        }
-        return "redirect:";
+/*
+    @GetMapping("/profile/allUsers")
+    public List<User> getAllEmployees() {
+        return (List<User>) userRepository.findAll();
     }
 
-    @PostMapping("/editProfile")
-    public RedirectView profile(@RequestParam String firstName, @RequestParam String lastName, @RequestParam File pic) {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof CustomUserDetails) {
-            User u = ((CustomUserDetails) principal).getUser();
-            u.setFirstName(firstName);
-            u.setLastName(lastName);
-            u.setPic(pic);
-            userRepository.save(u);
-        }
-        return new RedirectView("/editProfile");
+    @GetMapping("/profile/{id}")
+    public ResponseEntity<User> getEmployeeById(@PathVariable(value = "id") int userId)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
+        return ResponseEntity.ok().body(user);
+    }
+/*
+    @GetMapping("/profile/{name}")
+    public String profile() {
+        return "profile";
+    }
+    /*
+    @GetMapping("/profile/{id}")
+    public String profile(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("user", (User)userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id)));
+        return "profile";
+    }
+    */
+
+    @PostMapping("/profile")
+    public RedirectView profile(@RequestParam String firstName, @RequestParam String lastName){
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User u = userRepository.findByEmail(user.getUsername());
+        u.setFirstName(firstName);
+        u.setLastName(lastName);
+        userRepository.save(u);
+        return new RedirectView("/profile");
     }
 
-    @GetMapping(value = "/deleteUser/{userId}")
-    public String deleteUser(@PathVariable("userId") Integer userId) {
-        Optional<User> userOptional = userRepository.findById(userId);
-        if (userOptional.isPresent()) {
-            User u = userOptional.get();
-            u.setActive(false);
-            userRepository.save(u);
+   /* @PostMapping("/profile")
+    public RedirectView profile(@RequestParam String email, @RequestParam String firstName, @RequestParam String lastName){
+        Iterable<User> user = userRepository.findAll();
+        for(User u : user){
+            if(u.getEmail().equals(email)){
+                u.setFirstName(firstName);
+                u.setLastName(lastName);
+               // u.setPic(pic);
+                userRepository.save(u);
+            }
         }
-        return "registration";
-    }
+        return new RedirectView("/profile");
+    }*/
 
     @GetMapping("/userList")
-    public String userList(Model model) {
+    public String userList(Model model){
         model.addAttribute("user", userRepository.findAll());
         return "userList";
     }
 
-    @GetMapping(value = "/banUser/{userId}")
-    public RedirectView deleteUserForAdmin(@PathVariable("userId") Integer userId) {
-        Optional<User> userOptional = userRepository.findById(userId);
-        if (userOptional.isPresent()) {
-            User u = userOptional.get();
-            u.setActive(false);
-            u.setWasBanned(true);
-            userRepository.save(u);
-        }
-        return new RedirectView("/userList");
-    }
-
-    @GetMapping(value = "/unbanUser/{userId}")
-    public RedirectView resurrectUserForAdmin(@PathVariable("userId") Integer userId) {
-        Optional<User> userOptional = userRepository.findById(userId);
-        if (userOptional.isPresent()) {
-            User u = userOptional.get();
-            u.setActive(true);
-            u.setWasBanned(false);
-            userRepository.save(u);
-        }
-        return new RedirectView("/userList");
-    }
-
-    @GetMapping(value = "/userGroups")
-    public String getGroups(Model model) {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof CustomUserDetails) {
-            model.addAttribute("user", ((CustomUserDetails) principal).getUser())
-                    .addAttribute("group", groupRepository.findByUserId(((CustomUserDetails) principal).getUser().getId()));
-            return "userGroups";
-        }
-        return "redirect:";
-    }
-
-    @PostMapping(value = "/userGroups")
-    public RedirectView addGroup(@RequestParam String name) {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof CustomUserDetails) {
-            User u = ((CustomUserDetails) principal).getUser();
-            Group group = new Group();
-            group.setGroupName(name);
-            group.addUser(u);
-            groupRepository.save(group);
-        }
-        return new RedirectView("/userGroups");
-    }
-/*
-    @GetMapping(value = "/Group/{groupId}")
-    public String currentGroup(Model model, @PathVariable("groupId") Integer groupId) {
-        Group group = groupRepository.findByGroupId(groupId);
-        model.addAttribute("group", group);
-        return"Group";
-    }
-
- */
 }
